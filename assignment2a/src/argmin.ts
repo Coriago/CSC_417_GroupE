829044361eeb1912c03ae97f10ccb4f5ce5bb6ba
demills <demills@ncsu.edu>
2019-04-05 16:03:34 -0400

    Added some comments to argmin.ts.

--- assignment2a/src/argmin.ts
+++ assignment2a/src/argmin.ts
@@ -1,5 +1,27 @@
 import * as fs from "fs";
 
+/**
+ * @author: Daniel Mills ( demills )
+ * @file  : argmin.ts
+ *
+ * This script groups records (sorted numerically by their last column) of a 
+ * dataset (read from STDIN) into classes by:
+ *
+ *    1) Determining an index to split the records on such that aggregate
+ *       variance between records above and below the index is minimized.
+ *
+ *           i.e.: Records are split into two groups, those below and above
+ *           the index. Standard deviation is calculated for each group, and
+ *           the expected value of these two standard deviations is given.
+ *
+ *    2) Split the records on this index.
+ *    3) Repeat 1 and 2 on the records above the split index until either:
+ *           - There are sqrt( dataset_length) records above or 
+ *             below are the split index. ( minBinSize )
+ *           - The range of the values above or below the split index is  
+ *             less than (0.3 * stddev( dataset[ last_index ] )). ( minRise )
+ */
+
 // The column names of the input data.
 var attributes: Array<String>; 
 var numCols:number;
@@ -15,18 +37,41 @@ var lastCol:number[];
 // "lastCol" from i to "numRows".
 var allStdDevs:number[]; 
 
+// Minimum number of records allowed in a partition of the dataset.
+// The choice of split index depends on this value.
 var minBinSize:number;
+
+// Minumum range allowed for values of a partition of the subset.
+// The choice of split index depends on this value.
 var minRise:number;
+
+// A medium range "effect size" value. Used with the total standard 
+// deviation of the dataset's last column to determine "minRise".
 var COHEN:number = 0.3;
-var data: Array<String>; 
+
+/**
+ * Reads data, assuming CSV format, from STDIN into two arrays.
+ *   - "attributes" : A 1D array of strings; the column names. 
+ *   - "data"       : A 2D array of numbers; the raw data
+ */
 function readInput() {
-    data = fs.readFileSync( '/dev/stdin', 'utf8' ).split( "\n" );
+    var data:Array<String> = fs.readFileSync( '/dev/stdin', 'utf8' ).split( "\n" );
     attributes = data[ 0 ].split( "," );
     rows = data.slice( 1, data.length - 1).map( 
         line => line.split(",").map(Number)
     );
 }
 
+/**
+ * Given an "aggregate" of statistics (mean, stddev, size, etc.) about an 
+ * attribute (probably "dom"), numInc() is used to update these statistics 
+ * for a single additional value; i.e. A moving/rolling calculation of stats.
+ *
+ * "aggregate" has structure { count, mean, M2, sd, max, min }
+ *
+ * @param aggr:  A collection of statistics on the existing sample set.
+ * @param value: A new value added to the set.
+ */
 function numInc( aggr, value ) {
     // TODO maybe handle case where "value" is "?".
     aggr.count++; 
@@ -94,15 +139,11 @@ function calcSplits() {
                 numInc( belowSplitAggr, value );
                 numDec( aboveSplitAggr, value );
                 if ( ( belowSplitAggr.count >= minBinSize ) &&
-                     ( aboveSplitAggr.count >= minBinSize ) ) 
-                {
-                    if ( ( belowSplitAggr.max - belowSplitAggr.min ) > minRise ) 
-                    {
-                        if ( ( aboveSplitAggr.max - aboveSplitAggr.min ) > minRise ) 
-                        {
+                     ( aboveSplitAggr.count >= minBinSize ) ) {
+                    if ( ( belowSplitAggr.max - belowSplitAggr.min ) > minRise ) {
+                        if ( ( aboveSplitAggr.max - aboveSplitAggr.min ) > minRise ) {
                             tmp = getTotalVariance( belowSplitAggr, aboveSplitAggr ) * 1.05;
-                            if ( tmp < best ) 
-                            {
+                            if ( tmp < best ) {
                                 cut = split;
                                 best = tmp;
 
@@ -115,18 +156,18 @@ function calcSplits() {
         return cut + 1;
     }
 
-    var count:number = 0;
     var cut:number = 0;
-    var sym:string = "|..";
     var prevCut:number;
 
+    // Writes first line of output, the attribute names.
     for ( var i = 0; i < numCols - 1; i++ ) {
         process.stdout.write( attributes[ i ] + ", " );
     }
     process.stdout.write( attributes[ i ] + "\n" ); 
 
+    // Calculates the optimal cuts, then prints the input data with 
+    // the cuts as the last column. This for loop only prints 
     for ( var i = 0; i < numRows; i++ ) {
-        count++;
         if ( ( numRows - i ) > minBinSize ) {
             prevCut = cut;
             cut = argmin( i, numRows);
